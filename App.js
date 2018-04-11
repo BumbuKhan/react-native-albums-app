4fe9b6e71c1938ce437642eceb4b503720e368da
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-04-11 23:09:06 +0400

    Added a mockup for AlbumList component

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import Header from './src/components/header';
+import { View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
   return (
-    <Header title="Albums"/>
+    <View>
+      <Header title="Albums"/>
+      <AlbumList/>
+    </View>
   );
 }
 
