0954ef58a208c8711963671bb10624e830dc3dd1
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-04-11 22:44:23 +0400

    Added shadow to the header, also text has been centered

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Header = () => {
-  return <Text style={style.textStyle}>Albums</Text>
+  return (
+    <View style={style.viewStyle}>
+      <Text style={style.textStyle}>Albums</Text>
+    </View>
+  );
 }
 
 const style = StyleSheet.create({
   textStyle: {
     fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    shadowColor: '#e0e0e0',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   }
 });
 
