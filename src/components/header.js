ee398460c25e24e65f57df0138c64f065c0341be
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-04-11 22:23:50 +0400

    Added some styles to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 const Header = () => {
-  return <Text>Albums</Text>
+  return <Text style={style.textStyle}>Albums</Text>
 }
+
+const style = StyleSheet.create({
+  textStyle: {
+    fontSize: 20
+  }
+});
 
 export default Header;
